8e720f6b3a13be30370d9ac2098ed39c9b16102c
OliverG96 <oliverarafo@gmail.com>
2012-11-11 19:21:57 +0000

    Sure the build is fixed now

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -376,8 +376,8 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
-extern struct cpufreq_governor cpufreq_gov_smartassH3;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartassH3)
+extern struct cpufreq_governor cpufreq_governor_smartass_h3;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass_h3)
 #endif
 
 
